20d48f6cb1cb3163c47e6202b68feade651f269d
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2025-10-08 09:35:46 -0500

    feat: integrate @e2b/code-interpreter for sandbox management and update generateApp function to include sandbox URL retrieval

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,6 +1,8 @@
 import { inngest } from './client';
 import { createGoogleGenerativeAI } from '@ai-sdk/google';
 import { generateText } from 'ai';
+import { Sandbox } from '@e2b/code-interpreter';
+import { getSandbox } from './utils';
 
 const google = createGoogleGenerativeAI({
   apiKey: process.env.GEMINI_API_KEY
@@ -9,8 +11,13 @@ const google = createGoogleGenerativeAI({
 export const generateApp = inngest.createFunction(
   { id: 'generate-app' },
   { event: 'test/generate.app' },
-  async ({ event }) => {
+  async ({ event, step }) => {
     try {
+      const sandboxId = await step.run('get-sandbox-id', async () => {
+        const sandbox = await Sandbox.create('adorable-nextjs-2');
+        return sandbox.sandboxId
+      });
+
       const result = await generateText({
         model: google('gemini-2.0-flash-001'),
         prompt: `You are an Agent App Builder. Your goal is to build an Next.js app based on the user's prompt.
@@ -22,7 +29,14 @@ export const generateApp = inngest.createFunction(
       });
 
       console.log(result.text);
-      return { message: result.text };
+
+      const sandboxURL = await step.run('get-sandbox-url', async () => {
+        const sandbox = await getSandbox(sandboxId);
+        const host = sandbox.getHost(3000);
+        return `https://${host}`;
+      });
+
+      return { message: result.text, sandboxURL };
     } catch (error) {
       console.error(error);
       return { message: 'Error generating app' };
---
+++ src/inngest/utils.ts
@@ -0,0 +1,6 @@
+import { Sandbox } from '@e2b/code-interpreter';
+
+export async function getSandbox(sandboxId: string) {
+  const sandbox = await Sandbox.connect(sandboxId, { apiKey: process.env.E2B_API_KEY });
+  return sandbox;
+}
